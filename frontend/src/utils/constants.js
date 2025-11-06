c8fb0e59bec182cfaefbc5ed1338e72a1e697e5e
gautam-chudasama <gautam.chudasama@outlook.com>
2025-11-06 11:47:54 +0530

    url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
 // export const API_URL = "http://localhost:5000/api";
-export const API_URL = "https://linkedin-clone-chi-eosin.vercel.app/";
+// export const API_URL = "https://linkedin-clone-chi-eosin.vercel.app/";
+export const API_URL = "https://linkedin-clone-chi-eosin.vercel.app";
