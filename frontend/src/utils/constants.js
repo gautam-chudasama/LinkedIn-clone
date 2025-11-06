092652fce6fbd45a7812e2244ca0ee289079d230
gautam-chudasama <gautam.chudasama@outlook.com>
2025-11-06 12:05:24 +0530

    fix: update API_URL to include '/api' endpoint in api.js and constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:5000/api";
 // export const API_URL = "https://linkedin-clone-chi-eosin.vercel.app/";
-export const API_URL = "https://linkedin-backend-ui34.onrender.com";
+export const API_URL = "https://linkedin-backend-ui34.onrender.com/api";
